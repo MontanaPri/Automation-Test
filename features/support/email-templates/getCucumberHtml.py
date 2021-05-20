0541b4bdedded87721d4a7e5e07eed0e2c801f4b
MontanaPri <83788779+MontanaPri@users.noreply.github.com>
2021-05-20 11:26:34 -0400

    em commit to master

--- features/support/email-templates/getCucumberHtml.py
+++ features/support/email-templates/getCucumberHtml.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python3.4
+#!C:\Program Files (x86)\Python35-32\python.exe
 
 """
-This code lives in /var/lib/jenkins/workspace/BTRIS_Quick_Test_New/features/support/email-templates on the Jenkins servers.
+This code lives in C:\\\\Demo\\\\Automation-Test\\\\features\\\\support\\\\email-templates on the Jenkins servers.
 
-It gets executed in the Bug_Automation_Test job, as a Pre-Send Script which
+    It gets executed in the Redcap-Automation job, as a Pre-Send Script which
 is part of the email extenstion plugin which was added as part of that job.
 
-The Pre-Send script is:
+    The Pre-Send script is:
 
     def env = build.getEnvironment()
-    String b_id = env['BUILD_ID'];
+String b_id = env['BUILD_ID'];
 
-    // call the python code with the build id and obtain the results
-    def command = "/var/lib/jenkins/workspace/BTRIS_Quick_Test_New/features/support/email-templates/getCucumberHtml.py --buildid ${b_id}"
-    def proc = command.execute()
-    proc.waitFor()
-    def result = proc.in.text
+// call the python code with the build id and obtain the results
+def command = "C:\\\\Demo\\\\Automation-Test\\\\features\\\\support\\\\email-templates\\\\getCucumberHtml.py --buildid ${b_id}"
+def proc = command.execute()
+proc.waitFor()
+def result = proc.in.text
 
-    // set the content in the message
-    msg.setContent(result, "text/html;charset=utf-8")
-    msg.saveChanges()
+// set the content in the message
+msg.setContent(result, "text/html;charset=utf-8")
+msg.saveChanges()
 
 """
 
@@ -29,27 +29,27 @@ import argparse
 
 def buildOutput(table):
 
-    output = "<style>"
-    with open("/var/lib/jenkins/workspace/BTRIS_Quick_Test_New/features/support/email-templates/cucumber.css", "r") as fd:
-        output += fd.read()
-    output += "</style>"
+output = "<style>"
+with open("C:\\\\Demo\\\\Automation-Test\\\\features\\\\support\\\\email-templates\\\\cucumber.css", "r") as fd:
+output += fd.read()
+output += "</style>"
 
-    # adjust all the links from relative to absolute
-    alinks = table.findAll("a")
-    for link in alinks:
-        link['href'] = reportBaseUrl + link['href']
+# adjust all the links from relative to absolute
+alinks = table.findAll("a")
+for link in alinks:
+link['href'] = reportBaseUrl + link['href']
 
-    output += str(table)
+output += str(table)
 
-    return output
+return output
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--buildid")
 args = parser.parse_args()
 
-loginUrl = "https://cc0dbtrisdoc.cc.nih.gov:9090/login?from=%2F"
-reportBaseUrl = "https://cc0pbtrisdoc.cc.nih.gov:9090/job/BTRIS_Quick_Test_New/{0}/cucumber-html-reports/".format(args.buildid)
+loginUrl = "http://localhost:8080/login?from=%2F"
+reportBaseUrl = "http://localhost:8080/job/BTRIS_Test_Automation//{0}/cucumber-html-reports/".format(args.buildid)
 overview = "overview-features.html"
 
 # Browser
@@ -60,7 +60,7 @@ browser = mechanicalsoup.Browser(soup_config={ 'features': 'lxml'})
 login_page = browser.get(loginUrl, verify=False)
 login_page.raise_for_status()
 login_form = mechanicalsoup.Form(login_page.soup.select_one('form'))
-login_form.input({ "j_username": "jenkins", "j_password": "Docengine97@"})
+login_form.input({ "j_username": "Montana", "j_password": "jenkins"})
 
 login_result = browser.submit(login_form, login_page.url)
 
@@ -73,6 +73,5 @@ table = reportPage.soup.find("table", { "id" : "tablesorter" })
 
 
 report = buildOutput(table)
-
 
 print(report)
